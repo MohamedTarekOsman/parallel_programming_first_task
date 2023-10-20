d25afc8813d0f4e413c7900a8ff6673368064395
Mohamed Tarek <95485107+MohamedTarekOsman@users.noreply.github.com>
2023-10-20 19:46:55 +0300

    Add files via upload

--- parallel programming first task/test.c
+++ parallel programming first task/test.c
@@ -1,64 +1,20 @@
-// #include <stdio.h>
-// #include <string.h>
-// int main(void) {
-//     int userCount = 1;
-//     char usernames[100][10] = {"mohamed"};
-//     while (1) {
-//         printf("Enter 'register' to add a new user or 'login' to log in: ");
-//         char choice[10];
-//         scanf("%s", choice);
-//         if (strcmp(choice, "register") == 0) {
-//             if (userCount < 5) {
-//                 printf("Enter a new username: ");
-//                 char newUser[10];
-//                 scanf("%s", newUser);
-//                 // Check if the new username already exists
-//                 int usernameExists = 0;
-//                 for (int i = 0; i < userCount; i++) {
-//                     if (strcmp(newUser, usernames[i]) == 0) {
-//                         printf("Username already exists. Registration failed.\n");
-//                         usernameExists = 1;
-//                         break;
-//                     }
-//                 }
-//                 if (!usernameExists) {
-//                     strcpy(usernames[userCount], newUser);
-//                     userCount++;
-//                     printf("User registered successfully.\n");
-//                 }
-//             } else {
-//                 printf("User limit reached. Cannot register more users.\n");
-//             }
-//         } else if (strcmp(choice, "login") == 0) {
-//             char user[10];
-//             printf("Enter your username: ");
-//             scanf("%s", user);
-//             int flags = 0;
-//             for (int i = 0; i < userCount; i++) {
-//                 if (strcmp(user, usernames[i]) == 0) {
-//                     flags = 1;
-//                     break;
-//                 }
-//             }
-//             if (flags == 1) {
-//                 printf("Login successful. Welcome, %s!\n", user);
-//             } else {
-//                 printf("Login failed. User not found.\n");
-//             }
-//         } else {
-//             printf("Invalid choice. Please enter 'register' or 'login'.\n");
-//         }
-//     }
-//     return 0;
-// }
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+
+union Flag {
+    int value;
+};
+
+
+struct User {
+    char* username;
+    union Flag accessFlag;
+};
+
 int main(void) {
-    char** usernames = NULL;
+    struct User* users = NULL;
     int userCount = 0;
 
     while (1) {
@@ -71,10 +27,9 @@ int main(void) {
             printf("Enter a new username: ");
             scanf("%s", newUser);
 
-
             int usernameExists = 0;
             for (int i = 0; i < userCount; i++) {
-                if (strcmp(newUser, usernames[i]) == 0) {
+                if (strcmp(newUser, users[i].username) == 0) {
                     printf("Username already exists. Registration failed.\n");
                     usernameExists = 1;
                     break;
@@ -82,9 +37,16 @@ int main(void) {
             }
 
             if (!usernameExists) {
-                usernames = (char**)realloc(usernames, (userCount + 1) * sizeof(char*));
-                usernames[userCount] = (char*)malloc(10 * sizeof(char));
-                strcpy(usernames[userCount], newUser);
+                users = (struct User*)realloc(users, (userCount + 1) * sizeof(struct User));
+                users[userCount].username = (char*)malloc(10 * sizeof(char));
+                strcpy(users[userCount].username, newUser);
+
+                int flagValue;
+                printf("Enter the access flag value (1 for true, 0 for false): ");
+                scanf("%d", &flagValue);
+
+
+                users[userCount].accessFlag.value = flagValue;
                 userCount++;
                 printf("User registered successfully.\n");
             }
@@ -93,22 +55,33 @@ int main(void) {
             printf("Enter your username: ");
             scanf("%s", user);
 
-            int flags = 0;
+            int found = 0;
             for (int i = 0; i < userCount; i++) {
-                if (strcmp(user, usernames[i]) == 0) {
-                    flags = 1;
+                if (strcmp(user, users[i].username) == 0) {
+                    found = 1;
+                    // Check the accessFlag using integer value
+                    if (users[i].accessFlag.value == 1) {
+                        printf("Login successful. Welcome, %s!\n", user);
+                    } else {
+                        printf("Login failed. User not allowed.\n");
+                    }
                     break;
                 }
             }
 
-            if (flags == 1) {
-                printf("Login successful. Welcome, %s!\n", user);
-            } else {
+            if (!found) {
                 printf("Login failed. User not found.\n");
             }
         } else {
             printf("Invalid choice. Please enter 'register' or 'login'.\n");
         }
     }
+
+    // Free allocated memory
+    for (int i = 0; i < userCount; i++) {
+        free(users[i].username);
+    }
+    free(users);
+
     return 0;
 }
